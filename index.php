cca34f664e21a451c29851c2aca1d4e50d269966
maruian <maruan@floridauniversitaria.es>
2017-07-24 19:01:34 +0200

    Archivo index.php modificado

--- index.php
+++ index.php
@@ -4,9 +4,9 @@
   <head>
       <title>ACTIVIDAD 03</title>
       <?php
-        echo "Aquí poner vuestro nombre;
-        echo "Aquí poner vuestro primer apellido";
-        print "Aquí poner vuestro segundo apellido";
+        echo "Matias<br/>";
+        echo "Ruiz<br/>";
+        print "Andres<br/>";
       ?>
   </head>
   <body>
